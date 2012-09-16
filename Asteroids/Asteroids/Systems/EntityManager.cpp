245e616d0c78361fc288eb45deb9d715f89c17e6
Avenel <martin.briewig@googlemail.com>
2012-09-17 01:33:34 +0200

    EntityManager: Erste Verwaltungsfunktionen hinzugefuegt.

--- Asteroids/Asteroids/Systems/EntityManager.cpp
+++ Asteroids/Asteroids/Systems/EntityManager.cpp
@@ -1,11 +1,25 @@
 #include "EntityManager.h"
 
 
-EntityManager::EntityManager(void)
-{
+EntityManager::EntityManager(void) {
+	this->clientEntities = new hash_map<int, hash_map<int, list<Entity*>*> *>();
 }
 
 
-EntityManager::~EntityManager(void)
-{
+EntityManager::~EntityManager(void) {
+	delete this->clientEntities;
+}
+
+void EntityManager::addEntity(Entity *entity) {
+	this->clientEntities->find(entity->getClientId())->second->find(entity->getType())->second->push_back(entity);
+}
+
+Entity* EntityManager::getEntity(int id, int clientId, int type) {
+	list<Entity*> *clientList = this->clientEntities->find(clientId)->second->find(type)->second;
+	for (list<Entity*>::iterator it = clientList->begin(); it != clientList->end(); ++it) {
+		if ((*it)->getId() == id) {
+			return (*it);
+		}
+	}
+	return 0;
 }
--- Asteroids/Asteroids/Systems/EntityManager.h
+++ Asteroids/Asteroids/Systems/EntityManager.h
@@ -1,8 +1,25 @@
 #pragma once
+
+#include "..\Entity\Entity.h"
+#include <list>
+#include <hash_map>
+
 class EntityManager
 {
 public:
 	EntityManager(void);
 	~EntityManager(void);
+
+	void addEntity(Entity *newEntity);
+	Entity* getEntity(int id, int clientId, int type);
+	hash_map<int, Entity*>* getClientEntities(int clientId);
+
+private:
+	/** Es gibt 2 Hashmaps + 1 Liste:	1) Geordnet nach ClientIds
+										2) Geordnet nach Types
+										3) enthält Liste der Entities
+	*/
+			//clientid     type		entities      
+	hash_map<int, hash_map<int, list<Entity*>*> *> *clientEntities;
 };
 
