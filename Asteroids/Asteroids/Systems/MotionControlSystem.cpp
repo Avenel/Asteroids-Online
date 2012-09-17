85871857971375100870a49518c14c76aed8495f
soXx <soxx22@gmail.com>
2012-09-17 14:16:46 +0200

    Added: Aktion bei Keyboardeingabe

--- Asteroids/Asteroids/Systems/MotionControlSystem.cpp
+++ Asteroids/Asteroids/Systems/MotionControlSystem.cpp
@@ -9,3 +9,19 @@ MotionControlSystem::MotionControlSystem(void)
 MotionControlSystem::~MotionControlSystem(void)
 {
 }
+
+void MotionControlSystem::update() {
+	cout << "Taste gedrueckt" << endl;
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		cout << "Links" << endl;
+	}
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		cout << "Rechts" << endl;
+	}
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		cout << "Oben" << endl;
+	}
+	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+		cout << "Leertaste" << endl;
+	}	
+}
--- Asteroids/Asteroids/Systems/MotionControlSystem.h
+++ Asteroids/Asteroids/Systems/MotionControlSystem.h
@@ -1,8 +1,13 @@
 #pragma once
+#include <iostream>
+#include <SFML\Window\Keyboard.hpp>
+
+using namespace std;
 
 class MotionControlSystem
 {
 public:
 	MotionControlSystem(void);
 	~MotionControlSystem(void);
+	void update();
 };
