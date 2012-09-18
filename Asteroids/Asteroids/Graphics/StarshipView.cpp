ed4f84bd23e04296adabd847066140ed0d8791b6
soXx <soxx22@gmail.com>
2012-09-18 11:52:51 +0200

    fixed double to float konvertierung

--- Asteroids/Asteroids/Graphics/StarshipView.cpp
+++ Asteroids/Asteroids/Graphics/StarshipView.cpp
@@ -9,7 +9,7 @@ StarshipView::StarshipView(void)
 	this->setPointCount(3);
 	this->setPoint(0, sf::Vector2f(0,0));
 	this->setPoint(1, sf::Vector2f(0,this->size));
-	this->setPoint(2, sf::Vector2f(this->size,this->size/2.0));
+	this->setPoint(2, sf::Vector2f(this->size,this->size/2.0f));
 }
 
 
